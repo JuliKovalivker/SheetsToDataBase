1995f09c696b1a2d950ef5f2edd18901ba653501
Juli Kovalivker <julikovalivker@gmail.com>
2025-04-02 08:37:08 -0300

    feat: post

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ sh = client.open('BLCK_CAP')
 sh_ = sh.get_worksheet(1)
 sh_ = pd.DataFrame(sh_.get_all_values())
 lon = sh_.shape[0]
-obj = dict()
+obj = {"assets" : [],
+       "fundId": sh_[6][3]}
 
-url = 'https://app.kbinvesting.ai/' 
+url = 'http://localhost:3000/api/update-nav-value' 
 
 for i in range(3, lon-1):
-    obj[sh_[0][i]] = sh_[1][i]
-    print(sh_[0][i], obj[sh_[0][i]])
-    #x = requests.post(url, json = obj)
+    obj["assets"].append([sh_[0][i], sh_[1][i]])
+
+x = requests.post(url, json=obj)
