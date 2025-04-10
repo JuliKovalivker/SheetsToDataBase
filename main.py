43d6afb19293a09ce95fd66eb1ed636b9ce0e98a
Juli Kovalivker <julikovalivker@gmail.com>
2025-04-10 11:09:17 -0300

    feat: str_to_float

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import gspread
 import pandas as pd
 import requests
 
+def str_to_float(s):
+    return float(s.replace(',', ''))
+
 client = gspread.service_account(filename = 'credentials.json')
 sh = client.open('BLCK_CAP') 
 
@@ -9,11 +12,12 @@ sh_ = sh.get_worksheet(1)
 sh_ = pd.DataFrame(sh_.get_all_values())
 lon = sh_.shape[0]
 obj = {"assets" : [],
-       "fundId": sh_[6][3]}
+       "fundId": int(sh_[6][3])}
 
 url = 'http://localhost:3000/api/update-nav-value' 
+# url_ = 'http://localhost:3000/api/get-assets?fundId=2' 
 
 for i in range(3, lon-1):
-    obj["assets"].append([sh_[0][i], sh_[1][i]])
+    obj["assets"].append([sh_[0][i], str_to_float(sh_[1][i])])
 
 x = requests.post(url, json=obj)
